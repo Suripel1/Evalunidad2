2dddfa2331adb1acb69be349b25d6577035d3de0
Suripel1 <suripeli04@gmail.com>
2024-06-13 22:54:26 -0600

    actualizacion de archivo

--- gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio2.0-main/FrontEnd/Gimnasio-FrontEnd/src/components/Persona.vue
+++ gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio2.0-main/FrontEnd/Gimnasio-FrontEnd/src/components/Persona.vue
@@ -10,48 +10,48 @@
     <div class="grid gap-12 mb-12 sm:grid-cols-3">
         <div>
             <label for="Nombre"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Nombre</label>
             <input type="text" name="nombre" id="nombre"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="nombre" required="">
         </div>
         <div>
             <label for="Primer Apellido"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Primer apellido</label>
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Primer apellido</label>
             <input type="text" name="papellido" id="papellido"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="papellido" required="">
         </div>
         <div>
             <label for="Segundo Apellido"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Segundo apellido</label>
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Segundo apellido</label>
             <input type="text" name="sapellido" id="sapellido"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="sapellido">
         </div>
         <div>
             <label for="titulo"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titulo de cortesía</label>
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Titulo de cortesía</label>
             <input type="text" name="titulo" id="titulo"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Mr." required="">
         </div>
         <div>
             <label for="fechan"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fecha de nacimiento</label>
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Fecha de nacimiento</label>
             <input type="date" name="fechan" id="fechan"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500">
         </div>
         <div>
             <label for="foto"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Foto</label>
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Foto</label>
             <input type="file" name="titulo" id="titulo"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Mr." required="">
         </div>
         <div>
-            <label for="genero" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Selecciona el genero</label>
-            <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <label for="genero" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Selecciona el genero</label>
+            <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <option>Selecciona genero</option>
                 <option>Hombre</option>
                 <option>Mujer</option>
@@ -59,8 +59,8 @@
           </select>
         </div>
         <div>
-            <label for="sangre" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Selecciona tipo de sangre</label>
-            <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <label for="sangre" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Selecciona tipo de sangre</label>
+            <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <option>Selecciona tipo de sangre</option>
                 <option>A+</option>
                 <option>A-</option>
--- gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio2.0-main/FrontEnd/Gimnasio-FrontEnd/src/components/RegisterUser.vue
+++ gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio2.0-main/FrontEnd/Gimnasio-FrontEnd/src/components/RegisterUser.vue
@@ -13,48 +13,48 @@
                 <div class="grid gap-12 mb-12 sm:grid-cols-3">
                     <div>
                         <label for="Nombre"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Nombre</label>
                         <input type="text" name="nombre" id="nombre"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="nombre" required="">
                     </div>
                     <div>
                         <label for="Primer Apellido"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Primer apellido</label>
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Primer apellido</label>
                         <input type="text" name="papellido" id="papellido"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="papellido" required="">
                     </div>
                     <div>
                         <label for="Segundo Apellido"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Segundo apellido</label>
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Segundo apellido</label>
                         <input type="text" name="sapellido" id="sapellido"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="sapellido">
                     </div>
                     <div>
                         <label for="titulo"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titulo de cortesía</label>
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Titulo de cortesía</label>
                         <input type="text" name="titulo" id="titulo"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="Mr." required="">
                     </div>
                     <div>
                         <label for="fechan"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fecha de nacimiento</label>
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Fecha de nacimiento</label>
                         <input type="date" name="fechan" id="fechan"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     </div>
                     <div>
                         <label for="foto"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Foto</label>
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Foto</label>
                         <input type="file" name="titulo" id="titulo"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="Mr." required="">
                     </div>
                     <div>
-                        <label for="genero" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Selecciona el genero</label>
-                        <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <label for="genero" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Selecciona el genero</label>
+                        <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <option>Selecciona genero</option>
                             <option>Hombre</option>
                             <option>Mujer</option>
@@ -62,8 +62,8 @@
                       </select>
                     </div>
                     <div>
-                        <label for="sangre" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Selecciona tipo de sangre</label>
-                        <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <label for="sangre" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Selecciona tipo de sangre</label>
+                        <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <option>Selecciona tipo de sangre</option>
                             <option>A+</option>
                             <option>A-</option>
@@ -83,34 +83,34 @@
                 <div class="grid gap-12 mb-12 sm:grid-cols-3">
                     <div>
                         <label for="usuario"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Usuario</label>
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Usuario</label>
                         <input type="text" name="usuario" id="usuario"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="Username" required="">
                     </div>
                     <div>
                         <label for="password"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Password</label>
                         <input type="password" name="password" id="password"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="•••••••••" required="">
                     </div>
                     <div>
                         <label for="confirm-password"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Confirm
                             password</label>
                         <input type="password" name="confirm-password" id="confirm-password"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="•••••••••" required="">
                     </div>
                 </div>
                 <button type="submit"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    class="text-gray-900 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Registrar
                 </button>
                 <RouterLink to="/">
                 <button type="submit"
-                    class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                    class="text-gray-900 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
                     Cancelar
                 </button>
             </RouterLink>
