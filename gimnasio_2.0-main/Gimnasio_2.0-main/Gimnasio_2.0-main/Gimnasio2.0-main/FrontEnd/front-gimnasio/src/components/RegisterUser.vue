32613012dca2c14e2b97b36aefe4362ff937af56
Suripel1 <suripeli04@gmail.com>
2024-06-13 22:53:27 -0600

    modificacion de estilos

--- gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio2.0-main/FrontEnd/front-gimnasio/src/components/RegisterUser.vue
+++ gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio2.0-main/FrontEnd/front-gimnasio/src/components/RegisterUser.vue
@@ -10,7 +10,7 @@
         <Usuario></Usuario>
         <RouterLink to="/">
           <div class="w-full flex justify-center">
-          <button type="submit" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+          <button type="submit" class="text-gray-900 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
           Cancelar
           </button>
         </div>
