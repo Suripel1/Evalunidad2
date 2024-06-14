61cf3197996f1598c9474f91e110d40a3f203342
Suripel1 <suripeli04@gmail.com>
2024-06-13 20:15:10 -0600

    creacion de ruta para ejericios

--- gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio2.0-main/FrontEnd/front-gimnasio/src/router/index.js
+++ gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio2.0-main/FrontEnd/front-gimnasio/src/router/index.js
@@ -4,6 +4,7 @@ import RegisterUserView from '@/components/RegisterUser.vue'
 import DashboardView from '@/components/Dashboard.vue'
 import Persona from '@/components/Persona.vue'
 import PiePagina from '@/components/PiePagina.vue'
+import Ejercicios from '@/components/Ejercicios.vue'
 import Home from '@/components/Home.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +28,7 @@ const router = createRouter({
         {path:'/home', name: 'home', component:Home},
         {path:'/personas', name: 'personas', component:Persona},
         {path:'/piepagina', name: 'piepagina', component: PiePagina},
+        {path:'/ejercicios', name: 'ejercicios', component: Ejercicios},
       ]
     }
   ]
