554aaa87435f70d99662672171686e661452d3d9
Suripel1 <suripeli04@gmail.com>
2024-06-13 22:08:09 -0600

    modifcacion de archivo

--- gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio2.0-main/FrontEnd/front-gimnasio/src/components/ListarEjercicios.vue
+++ gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio2.0-main/FrontEnd/front-gimnasio/src/components/ListarEjercicios.vue
@@ -1,17 +1,19 @@
 <template>
     <div class="container">
-        <table class="table">
-            <thead class="thead-dark">
+       <table class="w-full text-m text-left rtl:text-right text-black dark:text-blue-100 custom-table">
+            <thead class="text-m text-white uppercase bg-black dark:text-white">
                 <tr>
                     <th scope="col">#</th>
-                    <th scope="col">Foto</th>
+                    <th scope="col">Video</th>
                     <th scope="col">Nombre</th>
-                    <th scope="col">Primer Apellido</th>
-                    <th scope="col">Segundo Apellido</th>
-                    <th scope="col">Titulo de Cortesia</th>
-                    <th scope="col">Fecha de Nacimiento</th>
-                    <th scope="col">Genero</th>
-                    <th scope="col">Tipo de Sangre</th>
+                    <th scope="col">Descricion</th>
+                    <th scope="col">Tipo</th>
+                    <th scope="col">Estatus</th>
+                    <th scope="col">Dificultad</th>
+                    <th scope="col">Fecha Registro</th>
+                    <th scope="col">Fecha Actualizacion</th>
+                    <th scope="col">Recomendaciones</th>
+                    <th scope="col">Restricciones</th>
                     <th scope="col">Eliminar</th>
                     <th scope="col">Editar</th>
                 </tr>
@@ -19,40 +21,52 @@
             <tbody>
                 <tr>
                     <th scope="row">1</th>
-                    <td><img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png" width="50px"/></td>
-                    <td>Norma</td>
-                    <td>Garcia</td>
-                    <td>Romero</td>
-                    <td>Mtra.</td>
-                    <td>19/06/1987</td>
-                    <td>Mujer</td>
-                    <td>O+</td>
+                    <td></td>
+                    <td>Peso Muerto</td>
+                    <td>consiste esencialmente en extender la cadera y los músculos que la rodean</td>
+                    <td>Aerobico</td>
+                    <td>Activo</td>
+                    <td>Avanzado</td>
+                    <td>31/Enero/2023</td>
+                    <td>31/Marzo/2023</td>
+                    <td>Requiere una importante activación del sistema nervioso central y 
+                    una gran implicación muscular, por tanto, se recomienda entre 1 y 2 sesiones pesadas a la 
+                    semana como máximo.</td>
+                    <td>Deberías hacer peso muerto al final el día de piernas y ponerlos primero el día de espalda.</td>
                     <td><a href="">Eliminar</a></td>
                     <td><a href="">Editar</a></td>
                 </tr>
                 <tr>
                     <th scope="row">2</th>
-                    <td><img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png" width="50px"/></td>
-                    <td>Daniel</td>
-                    <td>Garrido</td>
-                    <td>Hernandez</td>
-                    <td>Lic.</td>
-                    <td>19/06/1990</td>
-                    <td>Hombre</td>
-                    <td>A+</td>
+                    <td></td>
+                    <td>Peso Muerto</td>
+                    <td>consiste esencialmente en extender la cadera y los músculos que la rodean</td>
+                    <td>Aerobico</td>
+                    <td>Activo</td>
+                    <td>Avanzado</td>
+                    <td>31/Enero/2023</td>
+                    <td>31/Marzo/2023</td>
+                    <td>Requiere una importante activación del sistema nervioso central y 
+                    una gran implicación muscular, por tanto, se recomienda entre 1 y 2 sesiones pesadas a la 
+                    semana como máximo.</td>
+                    <td>Deberías hacer peso muerto al final el día de piernas y ponerlos primero el día de espalda.</td>
                     <td><a href="">Eliminar</a></td>
                     <td><a href="">Editar</a></td>
                 </tr>
                 <tr>
                     <th scope="row">3</th>
-                    <td><img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png" width="50px"/></td>
-                    <td>Maria Guadalupe</td>
-                    <td>Salas</td>
-                    <td>Cruz</td>
-                    <td>Ing.</td>
-                    <td>29/02/90</td>
-                    <td>Mujer</td>
-                    <td>B+</td>
+                    <td></td>
+                    <td>Peso Muerto</td>
+                    <td>consiste esencialmente en extender la cadera y los músculos que la rodean</td>
+                    <td>Aerobico</td>
+                    <td>Activo</td>
+                    <td>Avanzado</td>
+                    <td>31/Enero/2023</td>
+                    <td>31/Marzo/2023</td>
+                    <td>Requiere una importante activación del sistema nervioso central y 
+                    una gran implicación muscular, por tanto, se recomienda entre 1 y 2 sesiones pesadas a la 
+                    semana como máximo.</td>
+                    <td>Deberías hacer peso muerto al final el día de piernas y ponerlos primero el día de espalda.</td>
                     <td><a href="">Eliminar</a></td>
                     <td><a href="">Editar</a></td>
                 </tr>
