2c085a3a15c60ab349d8f01c9f764c7b7e4b60a0
Suripel1 <suripeli04@gmail.com>
2024-06-13 22:09:18 -0600

    modificacion de login

--- gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio2.0-main/FrontEnd/front-gimnasio/src/components/Login.vue
+++ gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio2.0-main/FrontEnd/front-gimnasio/src/components/Login.vue
@@ -1,11 +1,8 @@
 <template>
 <div class="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
-    <div class="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+    <div class="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1">
         <div class="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-            <div>
-                <img src="https://storage.googleapis.com/devitary-image-host.appspot.com/15846435184459982716-LogoMakr_7POjrN.png"
-                    class="w-32 mx-auto" />
-            </div>
+            
             <div class="mt-12 flex flex-col items-center">
                 <h1 class="text-2xl xl:text-3xl font-extrabold">
                     Registrar
@@ -65,7 +62,7 @@
                             type="password" placeholder="Contraseña" />
                         <RouterLink to="/dashboard">
                             <button
-                            class="mt-5 tracking-wide font-semibold bg-pink-500 text-gray-100 w-full py-4 rounded-lg hover:bg-pink-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                            class="mt-5 tracking-wide font-semibold bg-gray-900 text-red-100 w-full py-4 rounded-lg hover:bg-red-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
                             <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
                                 stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
@@ -79,7 +76,7 @@
                         </RouterLink>
                         <RouterLink to="/register">
                             <button
-                            class="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                            class="mt-5 tracking-wide font-semibold bg-gray-900 text-red-100 w-full py-4 rounded-lg hover:bg-red-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
                             <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
                                 stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
@@ -105,9 +102,9 @@
                 </div>
             </div>
         </div>
-        <div class="flex-1 bg-indigo-100 text-center hidden lg:flex">
-            <div class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
-                style="background-image: url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg');">
+        <div class="flex-1 bg-black text-center hidden lg:flex">
+            <div class="m-12 xl:m-10 w-full bg-contain bg-center bg-no-repeat"
+                style="background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXj2UBg7e6IOAoXOvQ3W7-EzoyjyqbfbSaC3DRWW1F-7wjHIEZlm4Kbyz8qOUj888sS1E&usqp=CAU');">
             </div>
         </div>
     </div>
